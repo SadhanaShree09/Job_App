7fcff9246e16eea9fc66df168d8d852ea3eb15cc
SadhanaShree09 <sadhanashreep90@gmail.com>
2025-08-01 16:09:35 +0530

    Added css and enhanced the logic of the Job Application Form

--- Job-Application/src/App.jsx
+++ Job-Application/src/App.jsx
@@ -13,24 +13,26 @@ function App() {
     alert(`Job Application submitted Successfully!!\n${JSON.stringify(data,null,2)}`);
   }
 
-  return (
-    <div className='text-center mt-10'>
-      <h2>Job Application Form</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div >
-          <label>FullName : </label>
+  return (  
+    <div className='max-w-2xl mx-auto mt-5 bg-white-50 rounded-lg shadow-lg mb-4'>
+      <h2 className='text-3xl font-bold mb-6 text-center underline text-gray-800'>Job Application Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className='space-y-6 mx-3 pb-6 bg'>
+        <div className=''>
+          <label className='block text-md font-medium text-gray-700 mb-1'>FullName : </label>
           <input
+            className='w-full border border-gray-300 p-2 rounded-md focus:ring-1 focus:ring-orange-300 focus:outline-none'
             {...register('FullName',{required: 'FullName is required'})}
             placeholder='Enter your full name'
           />
           {errors.FullName && (
-            <p className='text-red-500'>{errors.FullName.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.FullName.message}</p>
           )}
         </div>
         
         <div>
-          <label>Email ID : </label>
-          <input type='email' 
+          <label className='block text-md text-gray-700 font-medium mb-1'>Email ID : </label>
+          <input className='w-full border border-gray-300 p-2 rounded-md focus:ring-1 focus:ring-orange-300 focus:outline-none' 
+          type='email' 
           {...register('Email',
             {required:'Email is required',
               pattern:{
@@ -41,13 +43,14 @@ function App() {
           placeholder='Enter yout Email ID'
           />
           {errors.Email &&(
-            <p className='text-red-500'>{errors.Email.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.Email.message}</p>
           )}
         </div>
         
         <div>
-          <label>Phone Number : </label>
-          <input type="tel"
+          <label className='block text-md font-medium text-gray-700 mb-1'>Phone Number : </label>
+          <input className='w-full border border-gray-300 p-2 rounded-md focus:ring-1 focus:ring-orange-300 focus:outline-none' 
+          type="tel"
           {...register('Phone',{
             required : "Phone number is required",
             pattern :{
@@ -58,13 +61,14 @@ function App() {
           placeholder='Enter you Phone Number'
           />
           {errors.Phone &&(
-            <p className='text-red-500'>{errors.Phone.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.Phone.message}</p>
           )}
         </div>
         
         <div>
-          <label>Position : </label>
-          <select {...register('position', {required : 'Postion is required'})}>
+          <label className='block text-md font-medium text-gray-700 mb-1'>Position : </label>
+          <select className= 'w-full border border-gray-300 p-2 rounded-md focus:ring-1 focus:ring-orange-300 focus:outline-none'
+          {...register('position', {required : 'Postion is required'})}>
             <option value="">Select a position</option>
             <option value="frontend developer">Frontend Developer</option>
             <option value="backend developer">Backend Developer</option>
@@ -73,43 +77,45 @@ function App() {
             <option value="project manager">Project Manager</option>
           </select>
           {errors.position && (
-            <p className='text-red-500'>{errors.position.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.position.message}</p>
           )}
         </div>
         
         <div>
-          <label>Experience Level</label>
-          <div >
-            <label>
-              <input type="radio"
+          <label className='block text-md font-medium text-gray-700 mb-1'>Experience Level</label>
+          <div className='flex flex-col gap-2 pl-2'>
+            <label className='flex items-center gap-2'>
+              <input  
+              type="radio"
               {...register('experience',{required:'Select Experience Level'})} 
               value='entry'
               />
-              Entry level(0-1 years)
+              Entry level (0-1 years)
             </label>
-            <label>
+            <label className='flex items-center gap-2'>
               <input type="radio"
               {...register('experience',{required:'Select Experience Level'})}
               value='mid' 
               />
-              Middle Level(2-5 years)
+              Middle Level (2-5 years)
             </label>
-            <label>
+            <label className='flex items-center gap-2'>
               <input type="radio"
               {...register('experience',{required:'Select Experience Level'})}
               value='senior'
               />
-              Senior Level(5+ years)
+              Senior Level (5+ years)
             </label>
           </div>
           {errors.experience &&(
-            <p className='text-red-500'>{errors.experience.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.experience.message}</p>
           )}
         </div>
 
         <div>
-          <label>Upload your Resume</label>
-          <input type="file"
+          <label className='block text-md font-medium text-gray-700 mb-1'>Upload your Resume</label>
+          <input 
+          type="file"
           accept='.pdf'
           {...register('resume',{
             required:'Resume is required',
@@ -123,16 +129,17 @@ function App() {
               }
             }
           })} 
+          className='w-full border border-gray-300 p-2 rounded-md bg-white'
           />
-          <p>Only PDF format is accepted in the resume</p>
+          <p className='text-sm text-gray-600 mt-1'>Only PDF format is accepted in the resume</p>
           {errors.resume && (
-            <p className='text-red-500'>{errors.resume.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.resume.message}</p>
           )}
         </div>
 
         <div>
-          <label>Cover Letter</label>
-          <textarea
+          <label className='block text-md font-medium text-gray-700 mb-1'>Cover Letter</label>
+          <textarea className='w-full border border-gray-300 p-2 rounded-md focus:ring-1 focus:ring-orange-300 focus:outline-none'
           {...register('cover',{
             maxLength :{
               value : 500,
@@ -143,26 +150,31 @@ function App() {
           rows='4'
           />
           {errors.cover && (
-            <p className='text-red-500'>{errors.cover.message}</p>
+            <p className='text-red-500 text-sm mt-1'>{errors.cover.message}</p>
           )}
         </div>
 
-        <div>
-          <label>
+        <div className='flex items-start gap-2'>
+          <label className='block text-md font-medium text-gray-700'>
            <input
-            type='checkbox'
+            type='checkbox' 
             {...register('agreeTerms',{
               required:'Agree the terms and Conditions'
             })}
            />
-           <span>I Agree the terms and conditions and willing to get offer in your company.</span>
+           <span className='text-sm text-gray-700 mx-2'>I Agree the terms and conditions and willing to get offer in your company.</span>
           </label>
-          {errors.agreeTerms && (
-            <p className='text-red-500'>{errors.agreeTerms.message}</p>
-          )}
         </div>
+        {errors.agreeTerms && (
+            <p className='text-red-500 text-sm'>{errors.agreeTerms.message}</p>
+        )}
         
-        <button type='submit'>Submit Application</button>
+        <button 
+        className='w-full p-1 py-2 px-4 bg-red-100 text-gray-800 rounded-md hover:bg-green-100 transition duration-200'
+        type='submit'
+        >
+          Submit Application
+        </button>
       
       </form>
     </div>
